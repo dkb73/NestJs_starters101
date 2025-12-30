caffc65db39c7964cf6fa56b3aede9dce0da7156
Dhwanan Bharadva <dhwananbcse@gmail.com>
2025-12-30 19:30:09 +0530

    Phase 1: feat/ input sanitation using class validator

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, // Strips out properties that are NOT in the DTO
+    forbidNonWhitelisted: true, // Throws an error if extra properties are sent
+  }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
